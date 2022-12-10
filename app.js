0a11e26c5aedb628fdcf46a205015a13f0f4ac1e
Steven Ayvar <ayvarsteven@gmail.com>
2022-12-10 10:07:56 -0600

    manually adds book objects

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'The Call of Cthulhu',
+        author: 'H. P. Lovecraft',
+        pages: 420,
+        read: false
+    },
+
+    {
+        title: 'Alice in Wonderland',
+        author: 'Lewis Carroll',
+        pages: 200,
+        read: false
+    },
+
+    {
+        title: 'The Count of Monte Cristo',
+        author: 'Alexandre Dumas',
+        pages: 894,
+        read: false
+    },
+
+    {
+        title: 'The Art of War',
+        author: 'Sun Tzu',
+        pages: 288,
+        read: true
+    }
+
+];
 
 function Book() {
 
