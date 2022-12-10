7f58433d57ff3f4839d65d9f9b64152c51d44f53
Steven Ayvar <ayvarsteven@gmail.com>
2022-12-10 16:48:38 -0600

    adds test book

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 let myLibrary = [
     {
-        title: 'Call of Cthulhu',
-        author: 'H.P. Lovecraft',
-        pages: 420,
-        read: false
-    },
-
-    {
-        title: 'Alice in Wonderland',
-        author: 'Lewis Carroll',
-        pages: 200,
-        read: false
-    },
-
-    {
-        title: 'The Count of Monte Cristo',
-        author: 'Alexandre Dumas',
-        pages: 894,
-        read: false
-    },
-
-    {
-        title: 'The Art of War',
-        author: 'Sun Tzu',
-        pages: 288,
-        read: true
+        title: 'Steven Ayvar',
+        author: 'Steven Ayvar',
+        pages: 2000
     }
 ];
 
@@ -60,7 +38,7 @@ function displayBook() {
     })
 }
 
-// displayBook()
+displayBook()
 
 function Book() {
 
